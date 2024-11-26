480e2a111923dd5bd41528541057d484880e0f84
James Brooks <james@alt-three.com>
2024-11-26 14:19:26 +0000

    Use Plain widget

---
+++ plain.js
@@ -0,0 +1,36 @@
+(function(d, script) {
+    script = d.createElement('script');
+    script.async = false;
+    script.onload = function (){
+        Plain.init({
+            appId: 'liveChatApp_01JDM3PR254XBX2KT88YW09BBE',
+            links: [
+                {
+                    icon: 'book',
+                    text: 'View our docs',
+                    url: 'https://docs.vapor.build',
+                }, {
+                    icon: 'discord',
+                    text: 'Join our Discord',
+                    url: 'https://discord.com/invite/laravel',
+                }
+            ],
+            style: {
+                chatButtonColor: '#00B1E4',
+                chatButtonIconColor: '#ffffff',
+            },
+            threadDetails: {
+                labelTypeIds: ['liveChatApp_01JDM3PR254XBX2KT88YW09BBE'],
+            },
+        });
+    };
+    script.src = 'https://chat.cdn-plain.com/index.js';
+    d.getElementsByTagName('head')[0].appendChild(script);
+
+    document.querySelector("#navbar ul a[href='mailto:spark@laravel.com']").onclick = function (e) {
+        if (typeof window.Plain !== 'undefined') {
+            e.preventDefault();
+            Plain.open()
+        }
+    }
+}(document));
